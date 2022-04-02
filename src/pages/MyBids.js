59545d1ab659bb3a5da3013e3b4362b1e48d75dc
Ushan Fernando <ushansankalpafernando@gmail.com>
2022-04-02 14:02:50 +0530

    fixed bug

--- src/pages/MyBids.js
+++ src/pages/MyBids.js
@@ -1,12 +1,10 @@
-import React, { useState, Fragment,useEffect } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 
 import DataTable from "react-data-table-component";
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import UserSidebar from "../components/UserSidebar";
 import AdvertistmentCarousel from "../components/AdvertistmentCarousel";
 import Auth from "../Authentication/Auth";
-
-
 
 const customStyles = {
   rows: {
@@ -48,8 +46,8 @@ const data = [
 
 function MyBids() {
   const [isOpened, setIsOpened] = useState(false);
-  const [userType, setUserType] = useState(Auth.getUserLevel())
-  const [bids, setBids] = useState([])
+  const [userType, setUserType] = useState(Auth.getUserLevel());
+  const [bids, setBids] = useState([]);
   const [error, setError] = useState([]);
   const handleSidebar = () => {
     setIsOpened(!isOpened);
@@ -58,60 +56,72 @@ function MyBids() {
   const columns = [
     {
       name: "Price",
-      selector: (row) => row.amount +" /=" ,
+      selector: (row) => row.amount + " /=",
       sortable: true,
     },
     {
       name: `${userType == "Buyer" ? "Seller" : "Buyer"}`,
       selector: (row) => {
-        if(userType == "Buyer"){
-          return row.sellerName
-        }else{
-          return row.buyerName
+        if (userType == "Buyer") {
+          return row.sellerName;
+        } else {
+          return row.buyerName;
         }
       },
     },
     {
       name: "Status",
-      selector: (row) => <h4 className={row.status=='pending'?'text-yellow-600 capitalize':'text-green-500 capitalize'}>{row.status}</h4>,
+      selector: (row) => (
+        <h4
+          className={
+            row.status == "pending"
+              ? "text-yellow-600 capitalize"
+              : "text-green-500 capitalize"
+          }
+        >
+          {row.status}
+        </h4>
+      ),
       sortable: true,
     },
     {
       name: "See more",
-      selector: (row) => <a href={`viewSeller${row.sellerId}`} >View</a>,
+      selector: (row) => <a href={`viewSeller${row.sellerId}`}>View</a>,
       sortable: true,
     },
     {
       name: "Actions",
       cell: (row) =>
-        row.status == "pending" ? (
+        userType != "Buyer" && row.status == "pending" ? (
           <button
             className="bg-green-400 p-2 rounded-2xl text-white "
-            onClick={(e)=>acceptHandler(row._id,"confirmed",e)}
+            onClick={(e) => acceptHandler(row._id, "confirmed", e)}
             id={row._id}
           >
             Confirm
           </button>
-        ) : (
+        ) : userType != "Buyer" && row.status != "pending" ? (
           <button
             className="bg-orange-300 p-2 rounded-2xl text-white "
-            onClick={(e)=>acceptHandler(row._id,"pending",e)}
+            onClick={(e) => acceptHandler(row._id, "pending", e)}
             id={row._id}
           >
             Pending
           </button>
+        ) : (
+          <h1> N/A</h1>
         ),
       ignoreRowClick: true,
       allowOverflow: true,
       button: true,
-    }
+    },
   ];
 
   useEffect(() => {
     loadBids();
   }, []);
 
-  const acceptHandler = (id,status,e) => {
+  const acceptHandler = (id, status, e) => {
     e.preventDefault();
     fetch(`http://localhost:5000/api/bids/confirm`, {
       method: "POST",
@@ -119,7 +129,7 @@ function MyBids() {
         Accept: "application/json",
         "Content-Type": "application/json",
       }),
-      body: JSON.stringify({ id: id,status: status}),
+      body: JSON.stringify({ id: id, status: status }),
     })
       .then((res) => res.json())
       .then((response) => {
@@ -131,14 +141,19 @@ function MyBids() {
   };
 
   const loadBids = () => {
-    fetch(`http://localhost:5000/api/bids/${userType=="buyer"?'bids-buyer':'bids-seller'}?id=${Auth.getUserId()}`, {
-      method: "GET",
-      headers: new Headers({
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      }),
-      // body: JSON.stringify({ id: id }),
-    })
+    fetch(
+      `http://localhost:5000/api/bids/${
+        userType == "Buyer" ? "bids-buyer" : "bids-seller"
+      }?id=${Auth.getUserId()}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        }),
+        // body: JSON.stringify({ id: id }),
+      }
+    )
       .then((res) => res.json())
       .then((response) => {
         setBids(response);
@@ -147,7 +162,6 @@ function MyBids() {
       .catch((error) => console.log(error));
   };
 
-  
   return (
     <div className="h-screen flex">
       <UserSidebar
@@ -164,9 +178,11 @@ function MyBids() {
           <div className="-mt-16">
             <AdvertistmentCarousel />
           </div>
-          <br/>
+          <br />
           <div>
-            <h3 className=" text-2xl font-semibold mb-4 mt-20 w-full justify-center  text-center">My Bids</h3>
+            <h3 className=" text-2xl font-semibold mb-4 mt-20 w-full justify-center  text-center">
+              My Bids
+            </h3>
             <div className="w-4/5 mx-auto ">
               <DataTable
                 columns={columns}
